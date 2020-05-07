e5752ab4b8f8f69e4db109b10dfc25056eac9e12
Bartosz Biegajło <biegbart.z@gmail.com>
2020-05-07 15:25:55 +0200

    add combination and permutation without repetition

--- Combinatorial_Analysis/Combinatorial_Analysis.cpp
+++ Combinatorial_Analysis/Combinatorial_Analysis.cpp
@@ -59,21 +59,109 @@ void numInput(int& x)
     x = toInt(number);
 }
 
+enum class Operation {
+    PERMUTATION,
+    COMBINATION
+};
+
+Operation operationChoice()
+{
+    std::cout << "Select from available operations\n";
+    std::cout << "1. Permutation (without repetition)\n";
+    std::cout << "2. Combination (without repetition)\n";
+    std::cout << "(enter option number): ";
+
+    char option{};
+    while (true)
+    {
+        std::cin >> option;
+        switch (option)
+        {
+        case '1':
+            return Operation::PERMUTATION;
+        case '2':
+            return Operation::COMBINATION;
+        default:
+            std::cout << "Wrong input, try again: ";
+        }
+    }
+}
+
+int factorial(int x)
+{
+    if (x == 0)
+        return 1;
+
+    int i{ x };
+
+    while (i > 1)
+    {
+        --i;
+        x *= i;
+    }
+
+    return x;
+}
+
+int permutation(int x, int y)
+{
+    return factorial(x) / factorial(x - y);
+}
+
+int calculate(int x, int y, Operation op)
+{
+    int result{};
+    switch (op)
+    {
+    case Operation::PERMUTATION:
+        result = permutation(x, y);
+        break;
+    case Operation::COMBINATION:
+        result = permutation(x, y) / factorial(y);
+        break;
+    default:
+        break;
+    }
+
+    return result;
+}
+
+void printResult(int x, int y, int result, Operation op)
+{
+    switch (op)
+    {
+    case Operation::PERMUTATION:
+        std::cout << "A number of " << y << "-element permutations (without repetition) from "
+            << x << "-element set equals " << result << '\n';
+        break;
+    case Operation::COMBINATION:
+        std::cout << "A number of " << y << "-element combinations (without repetition) from "
+            << x << "-element set equals " << result << '\n';
+        break;
+    }
+}
+
 int main()
 {
+    // wrap everything in loop and ask for repetition
 
-    std::cout << "Enter two numbers\n";
-    std::cout << "First number: ";
+    // numbers input
+    std::cout << "Enter two natural numbers\n";
+    std::cout << "First number (n): ";
     int x{};
     numInput(x);
 
-    std::cout << "Second number: ";
+    std::cout << "Second number (k): ";
     int y{};
     numInput(y);
+    std::cout << std::endl;
 
     // calculation choice
+    Operation oper{ operationChoice() };
     // calculation execution
-    // result printing
+    int result{ calculate(x, y, oper) };
+    // print result
+    printResult(x, y, result, oper);
 
     // everyone happy
     
